a9878c9b2309b401c645156a46f287baf1561138
Guedes <gustavogueller@outlookcom>
2023-10-10 14:16:38 -0300

    gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath ("com.android.tools.build:gradle:7.0.0")
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.4")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id ("com.android.library") version "7.4.1" apply false
+    id ("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id ("com.google.dagger.hilt.android") version "2.47" apply false
+    id ("com.google.gms.google-services") version "4.4.0" apply false
 }
